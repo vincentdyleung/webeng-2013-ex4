b23afd673f5f7bd2a5bcdf6c7ffd7d71add37c22
Vincent Leung <vincentleung1991@gmail.com>
2013-05-21 14:34:44 +0200

    clear votes and set not confirmed when a trip is deleted

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -5,6 +5,15 @@ class Poi < ActiveRecord::Base
   validates :name, :location, :presence => true
   validates :name, :uniqueness => true
   
+  after_save :clear_votes_and_definitive
+  def clear_votes_and_definitive
+    if self.trip.nil?
+      Vote.find_all_by_poi_id(self.id).each do |vote|
+        vote.destroy
+      end
+    end
+  end
+  
   def <=> (other)
     Vote.find_all_by_poi_id(self.id).count <=> Vote.find_all_by_poi_id(other.id).count
   end
