739ec2029dc15a541c2fd59f073ad70a7e62f710
Vincent Leung <vincentleung1991@gmail.com>
2013-05-21 13:53:25 +0200

    sort POI based on popularity

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -4,4 +4,9 @@ class Poi < ActiveRecord::Base
   has_many :users, :through => :votes
   validates :name, :location, :presence => true
   validates :name, :uniqueness => true
+  
+  def <=> (other)
+    Vote.find_all_by_poi_id(self.id).count <=> Vote.find_all_by_poi_id(other.id).count
+  end
+  
 end
